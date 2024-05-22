926fa19a751cb4d434564b5782680d2064001052
Vladislav Shuklin <v.i.shuklin@gmail.com>
2024-05-22 19:18:35 +0300

    Exercise 3. Cart Page - custom message. Result
    A custom message on the yellow background is appearing if the total amount of the cart exceeds the amount, specified in the custom site preferences field.
    
    Co-Authored-By: Dharini-B <80411282+Dharini-B@users.noreply.github.com>
    Co-Authored-By: boris-kostov <97526771+boris-kostov@users.noreply.github.com>

---
+++ cartridges/app_custom_sfra_backend/cartridge/controllers/Cart.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var server = require('server');
+var page = module.superModule;
+server.extend(page);
+
+server.append('Show', function (req, res, next) {
+
+        var Site = require('dw/system/Site');
+        var BasketMgr = require('dw/order/BasketMgr');
+        var currentBasket = BasketMgr.getCurrentBasket();
+
+        var totalPriceCount = currentBasket.adjustedMerchandizeTotalPrice.value;
+        var priceToExceed = Site.current.getCustomPreferenceValue('cartTresholdAmount')
+
+        if (totalPriceCount >= priceToExceed) {
+            res.setViewData({
+                totalPriceCount : totalPriceCount,
+                priceToExceed: priceToExceed
+            });
+        }
+
+        next();
+
+    }
+);
+
+module.exports = server.exports();
