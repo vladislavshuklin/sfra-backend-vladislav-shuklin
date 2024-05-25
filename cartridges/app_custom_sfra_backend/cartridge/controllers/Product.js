90f95456019debdb0d48abe12723be640f0950ae
Vladislav Shuklin <v.i.shuklin@gmail.com>
2024-05-25 12:01:31 +0300

    Exercise 3. Code pretifier

--- cartridges/app_custom_sfra_backend/cartridge/controllers/Product.js
+++ cartridges/app_custom_sfra_backend/cartridge/controllers/Product.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-var server = require('server');
+var server = require("server");
 var page = module.superModule;
 server.extend(page);
 
-server.append('Show', function (req, res, next) {
-    var ProductMgr = require('dw/catalog/ProductMgr');
-    var CatalogMgr = require('dw/catalog/CatalogMgr');
+server.append("Show", function(req, res, next) {
+    var ProductMgr = rSequire("dw/catalog/ProductMgr");
+    var CatalogMgr = require("dw/catalog/CatalogMgr");
 
     var productId = req.querystring.pid;
     var productInfo = ProductMgr.getProduct(productId);
@@ -14,33 +14,37 @@ server.append('Show', function (req, res, next) {
     if (productInfo) {
         var primaryCategory = productInfo.primaryCategory;
         if (primaryCategory) {
-            var ProductSearchModel = require('dw/catalog/ProductSearchModel');
+            var ProductSearchModel = require("dw/catalog/ProductSearchModel");
             var searchModel = new ProductSearchModel();
-            var sortingRule = CatalogMgr.getSortingRule('price-low-to-high');
+            var sortingRule = CatalogMgr.getSortingRule("price-low-to-high");
             searchModel.setSortingRule(sortingRule);
             searchModel.setCategoryID(primaryCategory.ID);
             searchModel.search();
 
-            var categoryProducts = searchModel.getProductSearchHits().asList().slice(0, 5).toArray();
+            var categoryProducts = searchModel
+                .getProductSearchHits()
+                .asList()
+                .slice(0, 5)
+                .toArray();
             categoryProducts = categoryProducts.filter(function(i) {
                 return i.product.ID !== productInfo.ID;
             });
             categoryProducts = categoryProducts.slice(0, 4);
 
-            res.render('product/productDetails', {
+            res.render("product/productDetails", {
                 productInfo: productInfo,
-                primaryCategory : primaryCategory,
+                primaryCategory: primaryCategory,
                 categoryProducts: categoryProducts
             });
         } else {
-            res.render('product/productDetails', {
+            res.render("product/productDetails", {
                 productInfo: productInfo,
-                primaryCategory : primaryCategory,
+                primaryCategory: primaryCategory,
                 categoryProducts: []
             });
         }
     } else {
-        res.render('error/notFound');
+        res.render("error/notFound");
     }
     next();
 });
