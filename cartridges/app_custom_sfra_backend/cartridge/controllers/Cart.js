7ffcec8aa4421bedeb1ee17442001355c0542cd3
Vladislav Shuklin <v.i.shuklin@gmail.com>
2024-05-25 12:03:42 +0300

    Exercise 3. Code pretifier

--- cartridges/app_custom_sfra_backend/cartridge/controllers/Cart.js
+++ cartridges/app_custom_sfra_backend/cartridge/controllers/Cart.js
@@ -1,28 +1,25 @@
-'use strict';
+"use strict";
 
-var server = require('server');
+var server = require("server");
 var page = module.superModule;
 server.extend(page);
 
-server.append('Show', function (req, res, next) {
+server.append("Show", function(req, res, next) {
+    var Site = require("dw/system/Site");
+    var BasketMgr = require("dw/order/BasketMgr");
+    var currentBasket = BasketMgr.getCurrentBasket();
 
-        var Site = require('dw/system/Site');
-        var BasketMgr = require('dw/order/BasketMgr');
-        var currentBasket = BasketMgr.getCurrentBasket();
+    var totalPriceCount = currentBasket.adjustedMerchandizeTotalPrice.value;
+    var priceToExceed = Site.current.getCustomPreferenceValue("cartTresholdAmount");
 
-        var totalPriceCount = currentBasket.adjustedMerchandizeTotalPrice.value;
-        var priceToExceed = Site.current.getCustomPreferenceValue('cartTresholdAmount')
-
-        if (totalPriceCount >= priceToExceed) {
-            res.setViewData({
-                totalPriceCount : totalPriceCount,
-                priceToExceed: priceToExceed
-            });
-        }
-
-        next();
-
+    if (totalPriceCount >= priceToExceed) {
+        res.setViewData({
+            totalPriceCount: totalPriceCount,
+            priceToExceed: priceToExceed
+        });
     }
-);
+
+    next();
+});
 
 module.exports = server.exports();
